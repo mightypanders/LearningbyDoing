3b4653fdaacee533a8361d833009a5870a5acda2
Markus Dieckmann <Markus.Dieckmann@edataprocessing.de>
2015-11-30 09:56:29 +0100

    gameloop ausgelagert

--- Game/Game/Main.cs
+++ Game/Game/Main.cs
@@ -9,17 +9,23 @@ namespace Game
 {
     class MainLoop
     {
+        static gamestate GS;
 
         static void Main(string[] args)
         {
-            gamestate GS = new gamestate();
+            GS = new gamestate();
             Identifier MainID = new Identifier();
             Init init = new Init();
             init.PlayersInit(3);
+            gameloop();           
+        } 
+
+        static void gameloop()
+        {
             while (GS.state)
             {
 
             }
-        } 
+        }
     }
 }
