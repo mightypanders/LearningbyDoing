169b1274560ed1a5965ad39ae89d62fbd3092b53
Markus Dieckmann <Markus.Dieckmann@edataprocessing.de>
2016-01-04 16:24:43 +0100

    switch case commented

--- Rennbahn/Rennbahn/Form1.cs
+++ Rennbahn/Rennbahn/Form1.cs
@@ -21,20 +21,20 @@ namespace Rennbahn
         {
             switch (p.nummer)
             {
-                case 1:
-                    rb_1.Text = p.name + " hat " + p.Geld + "€";
-                    lbl_wette1.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
-                    break;
-                case 2:
-                    rb_2.Text = p.name + " hat " + p.Geld + "€";
-                    lbl_wette2.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
-                    break;
-                case 3:
-                    rb_3.Text = p.name + " hat " + p.Geld + "€";
-                    lbl_wette3.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
-                    break;
-                default:
-                    break;
+                //case 1:
+                //    rb_1.Text = p.name + " hat " + p.Geld + "€";
+                //    lbl_wette1.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
+                //    break;
+                //case 2:
+                //    rb_2.Text = p.name + " hat " + p.Geld + "€";
+                //    lbl_wette2.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
+                //    break;
+                //case 3:
+                //    rb_3.Text = p.name + " hat " + p.Geld + "€";
+                //    lbl_wette3.Text = p.name + " wettet " + p.MeineWette.Betrag + " € auf Hund " + p.MeineWette.Hund;
+                //    break;
+                //default:
+                //    break;
             }
         }
     }
