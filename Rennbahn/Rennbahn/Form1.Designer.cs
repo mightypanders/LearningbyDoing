d4133f3327c65edac578459cf9ec48f3c00e5c53
Markus Dieckmann <Markus.Dieckmann@edataprocessing.de>
2016-01-04 15:53:21 +0100

    Form1 Racetrack Hintergrund in resx

--- Rennbahn/Rennbahn/Form1.Designer.cs
+++ Rennbahn/Rennbahn/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pb_hintergrund = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_hintergrund)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_hintergrund
+            // 
+            this.pb_hintergrund.Image = global::Rennbahn.Properties.Resources.racetrack;
+            this.pb_hintergrund.Location = new System.Drawing.Point(12, 12);
+            this.pb_hintergrund.Name = "pb_hintergrund";
+            this.pb_hintergrund.Size = new System.Drawing.Size(743, 266);
+            this.pb_hintergrund.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_hintergrund.TabIndex = 0;
+            this.pb_hintergrund.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 469);
+            this.Controls.Add(this.pb_hintergrund);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_hintergrund)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_hintergrund;
     }
 }
 
