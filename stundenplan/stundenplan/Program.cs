7c4a9c1407b163f38ee3632555800718cb6a1c93
Markus Dieckmann <Markus.Dieckmann@edataprocessing.de>
2016-01-04 10:59:02 +0100

    Klasse Woche eingeführt, Array basierte Ausgabe

--- stundenplan/stundenplan/Program.cs
+++ stundenplan/stundenplan/Program.cs
@@ -24,12 +24,12 @@ namespace stundenplan
         #endregion
 
         #region Konstruktoren
-        public Tag (string WT, Stunde[] arr)
+        public Tag(string WT, Stunde[] arr)
         {
             wochentag = WT;
             Stundenarray = arr;
         }
-        public Tag (string WT, Stunde s1, Stunde s2, Stunde s3, Stunde s4, Stunde s5, Stunde s6)
+        public Tag(string WT, Stunde s1, Stunde s2, Stunde s3, Stunde s4, Stunde s5, Stunde s6)
         {
             wochentag = WT;
             Stundenarray[0] = s1;
@@ -48,13 +48,15 @@ namespace stundenplan
             for (int i = 0; i < Stundenarray.Length; i++)
             {
                 Stundenarray[i].druckeStunde();
+
             }
+            Console.WriteLine(" ");
         }
         #endregion
 
     }
-    class Stunde 
-    {   
+    class Stunde
+    {
         #region Member
         private string _fach;
         private string _lehrer;
@@ -62,7 +64,7 @@ namespace stundenplan
         #endregion
 
         #region Konstruktor
-        public Stunde(string fach, string lehrer, string raum) 
+        public Stunde(string fach, string lehrer, string raum)
         {
             _fach = fach;
             _lehrer = lehrer;
@@ -73,43 +75,73 @@ namespace stundenplan
         #region funktionen
         public void druckeStunde()
         {
-            Console.WriteLine("________");
-            Console.WriteLine(_fach);
-            Console.WriteLine(_lehrer);
-            Console.WriteLine(_raum);
-            
+            //Console.WriteLine("________");
+            Console.WriteLine(_fach + " " + _raum + " " + _lehrer);
+            //Console.WriteLine(_lehrer);
+            //Console.WriteLine(_raum);
+
         }
         #endregion
     }
+    class Woche
+    {
+        #region Member
+        int Wochennummer;
+        Tag[] Tagesarray;
+        #endregion
 
+        #region Konstruktoren
+        public Woche(int Wochennummer, Tag[] Tagesarray)
+        {
+            this.Wochennummer = Wochennummer;
+            this.Tagesarray = Tagesarray;
+        }
+
+        #endregion
+
+        #region Methoden
+
+        public void WocheDrucken()
+        {
+            for (int i = 0; i < Tagesarray.Length; i++)
+            {
+                Tagesarray[i].Tagausgeben();
+            }
+        }
+        #endregion
+    }
     class Program
     {
-       
+
         static Stunde AW = new Stunde("AW", "Wittek", "HNN 106");
         static Stunde IT = new Stunde("IT", "Forck", "HNE 303");
         static Stunde WGW = new Stunde("WG", "Wingold", "HNE 202");
-        static Stunde WGA = new Stunde("WG", "Abel", "HNE205");
-        static Stunde DE = new Stunde("DE", "Trieber-Schreyer", "HNN202");
-        static Stunde REL = new Stunde("REL", "Hinz", "HNN206");
-        static Stunde WGE = new Stunde("WGE", "Engels", "HNE303");
-        static Stunde EN = new Stunde("EN", "Mühlenhoff", "HNE203");
+        static Stunde WGA = new Stunde("WG", "Abel", "HNE 205");
+        static Stunde DE = new Stunde("DE", "Trieber-Schreyer", "HNN 202");
+        static Stunde REL = new Stunde("RE", "Hinz", "HNN 206");
+        static Stunde WGE = new Stunde("WG", "Engels", "HNE 303");
+        static Stunde EN = new Stunde("EN", "Mühlenhoff", "HNE 203");
 
 
         static void Main(string[] args)
         {
-           
-            Tag mon = new Tag("Montag", AW, AW, IT, WGW, WGW, WGW);
-            Tag die = new Tag("Dienstag", IT, IT, DE, DE, WGA, WGA);
+            Tag[] Tage = TageAnlegen();
+            Woche W = new Woche(1, Tage);
+            W.WocheDrucken();
             //Tag mit = new Tag("Mittwoch",)
-            WocheAusgeben(mon, die);
-            Console.ReadKey();          
-        } 
 
-        public static void WocheAusgeben(Tag mon, Tag die)
-        {
-            mon.Tagausgeben();
-            die.Tagausgeben();
-            return;
+            Console.ReadKey();
         }
+        public static Tag[] TageAnlegen()
+        {
+            Tag[] Tage = new Tag[5];
+            Tage[0] = new Tag("Montag", AW, AW, IT, WGW, WGW, WGW);
+            Tage[1] = new Tag("Dienstag", IT, IT, DE, DE, WGA, WGA);
+            Tage[2] = new Tag("Mittwoch", IT, IT, DE, DE, WGA, WGA);
+            Tage[3] = new Tag("Donnerstag", AW, AW, IT, WGW, WGW, WGW);
+            Tage[4] = new Tag("Freitag", IT, IT, DE, DE, WGA, WGA);
+            return Tage;
+        }
+
     }
 }
