6a83c71cb572d156676f9a468f988e31e14f41f6
Markus Dieckmann <Markus.Dieckmann@edataprocessing.de>
2016-02-15 15:01:02 +0100

    Initial Commit, Abendessen Klassen Basis

---
+++ CateringKostenRechner/CateringKostenRechner/Abendessen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CateringKostenRechner
+{
+    class Abendessen
+    {
+        private int Personenanzahl = 0;
+        private decimal GKproPers = 0.0M;
+        private decimal dekokosten = 0.0M;
+
+        public const int EssenskostenproPerson = 25;
+    }
+}
