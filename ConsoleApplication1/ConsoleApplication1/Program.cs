ee5c4f5e604c3028a758106d4d0ce9a09309130c
mightypanders <mighty.panders@gmail.com>
2016-04-05 10:59:39 +0200

    Rename Project modulo

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 23;
+            Console.WriteLine(i++);
+            Console.WriteLine(++i);
+            Console.WriteLine(i+1 );
+
+            i *= 7;
+            Console.WriteLine(i);
+            Console.WriteLine(i/100);
+            Console.WriteLine(i%100);
+            Console.ReadKey();
+        }
+    }
+}
