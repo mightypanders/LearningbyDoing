bebb1c1403dd1682c1103012f7fdb175cf313e1b
mightypanders <mighty.panders@gmail.com>
2016-05-09 17:14:58 +0200

    operatoren übung aus schule überprüft und eingefügt

---
+++ operatoren/operatoren/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatoren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 23;
+            Console.WriteLine(i++);
+            Console.WriteLine(++i);
+            Console.WriteLine(i+1);
+            Console.WriteLine(i);
+
+            i *= 7;
+
+            Console.WriteLine(i);
+            Console.WriteLine(i/100);
+            Console.WriteLine(i%100);
+
+            Console.ReadKey();
+        }
+    }
+}
