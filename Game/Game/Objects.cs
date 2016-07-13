0b8db30f7288d52ee969d5aba9f47f3ec08983bd
Markus Dieckmann <markus.dieckmann@edataprocessing.de>
2016-07-13 15:29:22 +0200

    stopper snippet noloops game Objects refactor

--- Game/Game/Objects.cs
+++ Game/Game/Objects.cs
@@ -10,17 +10,17 @@ namespace Game
 {
     //introducing a change
 
-    class Objects
+    class GameObject
     {
         protected string _name;
         protected int _uID;
 
-        public Objects()
+        public GameObject()
         {
             _name = null;
             _uID = 0;
         }
-        public Objects(Identifier ID, string name)
+        public GameObject(Identifier ID, string name)
         {
             _name = name;
             _uID = ID.getuID();
@@ -48,14 +48,14 @@ namespace Game
             }
         }
     }
-    class PowerUps : Objects
+    class PowerUp : GameObject
     {
         protected string _title;
         protected float _factor;
         protected int _amount;
 
 
-        public PowerUps(Identifier ID, string name, string title, float factor, int amount) : base(ID, name)
+        public PowerUp(Identifier ID, string name, string title, float factor, int amount) : base(ID, name)
         {
             //_uID = ID.getuID();
             //_name = name;
@@ -65,19 +65,19 @@ namespace Game
             //_uID = ID.getuID();
         }
     }
-    class Player : Objects
+    class Player : GameObject
     {
 
         protected short _playernumber;
         protected string _color;
-        protected PowerUps[] _powerUps;
+        protected PowerUp[] _powerUps;
         protected short _lives;
         protected short _wins;
         protected short _losses;
         protected int _posX;
         protected int _posY;
 
-        public Player(Identifier ID, string name, short playernumber, string color, PowerUps[] powerups, short lives, short wins, short losses, int posX, int posY) : base(ID, name)
+        public Player(Identifier ID, string name, short playernumber, string color, PowerUp[] powerups, short lives, short wins, short losses, int posX, int posY) : base(ID, name)
         {
             //_uID = ID.getuID();
             //_name = name;
