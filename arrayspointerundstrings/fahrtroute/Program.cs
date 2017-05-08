ef099b20ae9a04187dc67b8ba136f25c915f9bce
mightypanders <mighty.panders@gmail.com>
2017-05-08 13:41:30 +0200

    added fahrtrouten

---
+++ arrayspointerundstrings/fahrtroute/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fahrtroute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] d ={ { 0, 10, 20 }, { 10, 0, 15 }, { 20, 15, 0 } };
+            
+        }
+    }
+}
