fd25ab66e5ffa94036cbe883b970a9cce419754f
mightypanders <mighty.panders@gmail.com>
2017-05-09 10:47:27 +0200

    personenliste

--- Serialisierung/Serialisierung/Program.cs
+++ Serialisierung/Serialisierung/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Serialisierung
 {
@@ -22,8 +19,14 @@ namespace Serialisierung
                         perslist.Add(perseingabe());
                         break;
                     case 2:
+                        allepersonenausgeben();
                         break;
                     case 3:
+                        Person suche = suchePerson();
+                        personausgeben(suche);
+                        break;
+                    case 4:
+                        running = false;
                         break;
                     case 0:
                         Console.WriteLine("keine valide option");
@@ -34,24 +37,49 @@ namespace Serialisierung
             }
         }
 
+        private static Person suchePerson()
+        {
+            Console.WriteLine("geben sie den zu suchenden nachnamen ein:");
+            String suchname = Console.ReadLine();
+            if (!String.IsNullOrEmpty(suchname))
+                foreach (Person item in perslist)
+                    if (item.nachname.Contains(suchname))
+                        return item;
+            return new Person();
+        }
+
+        private static void personausgeben(Person item)
+        {
+            if (item.nachname != null)
+                Console.WriteLine(item.vorname + " / " + item.nachname + " / " + item.email + " / " + item.tel + " / " + item.alter);
+            else
+                Console.WriteLine("Kann keine Person ausgeben");
+        }
+
+        private static void allepersonenausgeben()
+        {
+            foreach (Person item in perslist)
+                personausgeben(item);
+        }
+
         private static int eingabeholen()
         {
-            int[] opt = new int[] { 1, 2, 3 };
+            int[] opt = new int[] { 1, 2, 3, 4 };
             int read = 0;
             Console.WriteLine("Wählen sie eine Alternative:");
             Console.WriteLine("1: eingabe");
             Console.WriteLine("2: ausgabe");
             Console.WriteLine("3: suche");
+            Console.WriteLine("4: beenden");
             int.TryParse(Console.ReadLine(), out read);
             if (opt.Contains(read))
-            {
                 return read;
-            }
             return 0;
         }
 
         private static Person perseingabe()
         {
+            Console.Clear();
             Person outPers = new Person();
             outPers.personennr = (uint)(perslist.Count);
             Console.WriteLine("geben sie einen Vornamen ein");
